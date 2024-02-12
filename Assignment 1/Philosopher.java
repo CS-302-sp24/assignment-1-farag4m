d77a8037e09314feeaea6430ccc5ed25b05deb14
farag4m <143025936+farag4m@users.noreply.github.com>
2024-02-11 23:46:16 -0600

    Update Philosopher.java

--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -4,8 +4,10 @@ class Philosopher extends Thread {
   private Chopstick left, right;
   private Random random;
   private int thinkCount;
+  private int id;
 
-  public Philosopher(Chopstick left, Chopstick right) {
+  public Philosopher(int id, Chopstick left, Chopstick right) {
+    this.id = id;
     this.left = left; this.right = right;
     random = new Random();
   }
@@ -17,6 +19,10 @@ class Philosopher extends Thread {
         if (thinkCount % 10 == 0)
           System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
         Thread.sleep(random.nextInt(1000));     // Think for a while
+
+        Chopstick firstChopstick = (left.getId() < right.getId()) ? left : right;
+        Chopstick secondChopstick = (left.getId() < right.getId()) ? right : left;
+        
         synchronized(left) {                    // Grab left chopstick 
           synchronized(right) {                 // Grab right chopstick 
             Thread.sleep(random.nextInt(1000)); // Eat for a while
