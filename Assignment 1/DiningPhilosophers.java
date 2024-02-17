f4ce37f0433f2a5734a92b74c0105c9675e0bea6
farag4m <143025936+farag4m@users.noreply.github.com>
2024-02-16 23:33:25 -0600

    Update DiningPhilosophers.java

--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -1,6 +1,12 @@
 public class DiningPhilosophers {
 
   public static void main(String[] args) throws InterruptedException {
+
+    if (args.length != 5) {
+      System.out.println("Usage: java DiningPhilosophers <np> <nc> <tt> <et> <rl>");
+      return;
+    }
+    
     int np = Integer.parseInt(args[0]);
     int nc = Integer.parseInt(args[1]);
     int tt = Integer.parseInt(args[2]);
@@ -13,8 +19,18 @@ public class DiningPhilosophers {
     
     for (int i = 0; i < np; ++i)
       chopsticks[i] = new Chopstick(i);
+    
     for (int i = 0; i < np; ++i) {
-      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]);
+      Chopstick leftChopstick = Chopsticks[i];
+      Chopstick rightChopstick = chopsticks[(i+1) % np];
+
+      // If philosophers are left-handed, swap
+      if (rl == 1 && i % 2 != 0){
+        leftChopstick = chopsticks[(i+1) % np];
+        rightChopstick = chopsticks[i];
+      }
+      
+      philosophers[i] = new Philosopher(i, leftChopstick, rightChopstick, nc, tt, et, rl);
       philosophers[i].start();
     }
     for (int i = 0; i < np; ++i)
