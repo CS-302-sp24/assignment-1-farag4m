ace33545833be4ccc2226a7f759379c5947cce16
farag4m <143025936+farag4m@users.noreply.github.com>
2024-02-16 23:46:55 -0600

    Update Philosopher.java

--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -5,10 +5,20 @@ class Philosopher extends Thread {
   private Random random;
   private int thinkCount;
   private int id;
+  private int nc;
+  private int tt;
+  private int et;
+  private int rt;
 
-  public Philosopher(int id, Chopstick left, Chopstick right) {
+  public Philosopher(int id, Chopstick left, Chopstick right, int nc, int tt, int et, int rl) {
     this.id = id;
-    this.left = left; this.right = right;
+    this.left = left; 
+    this.right = right;
+    this.nc = nc;
+    this.tt = tt;
+    this.et = et;
+    this.rl = rl;
+    
     random = new Random();
   }
 
@@ -17,17 +27,23 @@ class Philosopher extends Thread {
       while(true) {
         ++thinkCount;
         if (thinkCount % 10 == 0)
-          System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
-        Thread.sleep(random.nextInt(1000));     // Think for a while
+          System.out.println("Philosopher " + id + " has thought " + thinkCount + " times");
+        Thread.sleep(random.nextInt(tt));     // Think for a while
 
-        Chopstick firstChopstick = (left.getId() < right.getId()) ? left : right;
-        Chopstick secondChopstick = (left.getId() < right.getId()) ? right : left;
-        
-        synchronized(left) {                    // Grab left chopstick 
-          synchronized(right) {                 // Grab right chopstick 
-            Thread.sleep(random.nextInt(1000)); // Eat for a while
+        Chopstick rightChopstick = (rl == 0 || id % 2 == 0) ? left : right;
+        Chopstick leftChopstick = (rl == 0 || id % 2 == 0) ? right : left;
+
+        System.out.println("Philosopher " + id + " wants " + rightChopstick + " chopstick");
+        synchronized(rightChopstick) {                    // Grab right chopstick 
+          System.out.println("Philosopher " + id + " has " + rightChopstick + " chopstick");
+          System.out.println("Philosopher " + id + " wants " + leftChopstick + " chopstick");
+          synchronized(leftChopstick) {                 // Grab left chopstick 
+            System.out.println("Philosopher " + id + " has " + leftCHopstick + "chopstick");
+            Thread.sleep(random.nextInt(et)); // Eat for a while
           }
+          System.out.println("Philosopher " + id + " releases " + leftCHopstick + " chopstick");
         }
+        System.out.println("Philosopher " + id + " releases " + rightChopsticks + " chopsticks");
       }
     } catch(InterruptedException e) {}
   }
